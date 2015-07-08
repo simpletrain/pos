013bf3da17e647927893b7d6bed395bbc2f8ac41
simpletrain <zsy498761554@live.com>
2015-07-08 22:40:44 +0800

    pos_v0,refactored.

--- pos_v0/main/main.js
+++ pos_v0/main/main.js
@@ -1,19 +1,24 @@
+function getPosBody(inputsArray) {
+  var body = '';
+  var sumPrice = 0;
+  for (var i = 0; i < inputsArray.length; i++) {
+    body += '名称：' + inputsArray[i].name
+      + '，数量：' + inputsArray[i].count + inputsArray[i].unit
+      + '，单价：' + inputsArray[i].price.toFixed(2) + '(元)'
+      + '，小计：' + (inputsArray[i].count * inputsArray[i].price).toFixed(2) + '(元)\n';
+    sumPrice += inputsArray[i].count * inputsArray[i].price;
+  }
+  return {posBody: body, totalCost: sumPrice};
+}
 function printReceipt(inputs) {
   var outputs;
+  var totalCost = getPosBody(inputs).totalCost;
   var posHead = '***<没钱赚商店>收据***\n';
-  var posBody = '';
-  var totalCost = 0;
-  for (var i = 0; i < inputs.length; i++) {
-    posBody += '名称：' + inputs[i].name
-      + '，数量：' + inputs[i].count + inputs[i].unit
-      + '，单价：' + inputs[i].price.toFixed(2) + '(元)'
-      + '，小计：' + (inputs[i].count * inputs[i].price).toFixed(2) + '(元)\n';
-    totalCost += inputs[i].count * inputs[i].price;
-  }
+  var posBody = getPosBody(inputs).posBody;
   var posLine = '----------------------\n';
   var posTotalCost = '总计：' + totalCost.toFixed(2) + '(元)\n';
   var posEnd = '**********************';
   outputs = posHead + posBody + posLine + posTotalCost + posEnd;
   console.log(outputs);
 }
-//需要重构 分离函数
+//遍历两次？？
