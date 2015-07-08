27e49286e345ea3923dffe88ea1e86e28d1b3405
simpletrain <zsy498761554@live.com>
2015-07-08 18:39:29 +0800

    finish pos_v0,to be refactored.

--- pos_v0/main/main.js
+++ pos_v0/main/main.js
@@ -1,4 +1,19 @@
-//TODO: Please write code in this file.
 function printReceipt(inputs) {
-
+  var outputs;
+  var posHead = '***<没钱赚商店>收据***\n';
+  var posBody = '';
+  var totalCost = 0;
+  for (var i = 0; i < inputs.length; i++) {
+    posBody += '名称：' + inputs[i].name
+      + '，数量：' + inputs[i].count + inputs[i].unit
+      + '，单价：' + inputs[i].price.toFixed(2) + '(元)'
+      + '，小计：' + (inputs[i].count * inputs[i].price).toFixed(2) + '(元)\n';
+    totalCost += inputs[i].count * inputs[i].price;
+  }
+  var posLine = '----------------------\n';
+  var posTotalCost = '总计：' + totalCost.toFixed(2) + '(元)\n';
+  var posEnd = '**********************';
+  outputs = posHead + posBody + posLine + posTotalCost + posEnd;
+  console.log(outputs);
 }
+//需要重构 分离函数
